43f075ea8bac43e68c9eee6664e1aaff9d959308
Zihadul Islam <ahmedxihad@gmail.com>
2023-11-21 13:31:11 +0600

    Implement academic department

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { academicDepartmentRoutes } from '../modules/academicDepartment/academicDepartment.routes';
 import { AcademicFacultyRoutes } from '../modules/academicFaculty/academicFaculty.routes';
 import { AcademicSemesterRoutes } from '../modules/academicSemester/academicSemester.routes';
 
@@ -13,6 +14,10 @@ const moduleRoutes = [
   {
     path: "/academic-faculties",
     route: AcademicFacultyRoutes
+  },
+  {
+    path: "/academic-departments",
+    route: academicDepartmentRoutes
   }
 ];
 
